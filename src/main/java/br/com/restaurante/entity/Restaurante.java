508af0e503f4ec765f5fc219cd60a765ac0f0838
Davidd-Rodrigues <davideaf2008@hotmail.com>
2025-08-19 21:56:18 -0300

    fiz Retaurante  e repository

---
+++ src/main/java/br/com/restaurante/entity/Restaurante.java
@@ -0,0 +1,45 @@
+package br.com.restaurante.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Restaurante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column (nullable = false)
+    private String nome;
+
+    @Column (nullable = false, unique = true)
+    private String cnpj;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+
+}
