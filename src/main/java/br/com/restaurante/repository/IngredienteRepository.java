7638bd493a2cae7de56f324ac7eee71659ecf245
Rafael <rfledvaldo@gmail.com>
2025-08-26 20:45:09 -0300

    Classe e repositório feitos

--- src/main/java/br/com/restaurante/repository/IngredienteRepository.java
+++ src/main/java/br/com/restaurante/repository/IngredienteRepository.java
@@ -1,17 +1,12 @@
 package br.com.restaurante.repository;
 
-<<<<<<< HEAD
 import br.com.restaurante.entity.Ingrediente;
-=======
->>>>>>> 9755e812293aeb53912d756e287ced7d6852eb04
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-<<<<<<< HEAD
-public interface IngredienteRepository extends CrudRepository<Ingrediente,Long>{
-=======
-public interface IngredienteRepository
-    extends CrudRepository<Ingrediente,Long>{
->>>>>>> 9755e812293aeb53912d756e287ced7d6852eb04
+public interface IngredienteRepository extends CrudRepository<Ingrediente,Long> {
 }
+
+
