bed229d5303d603dbd97268981942b1b524ca086
Your Name <>
2025-08-26 20:10:22 -0300

    Ajuste em ingrediente reposito

--- src/main/java/br/com/restaurante/repository/IngredienteRepository.java
+++ src/main/java/br/com/restaurante/repository/IngredienteRepository.java
@@ -1,10 +1,9 @@
 package br.com.restaurante.repository;
 
-import br.com.restaurante.entity.SessaoClienteMesa;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IngredienteRepository
-        extends CrudRepository<SessaoClienteMesa,Long> {
+    extends CrudRepository<Ingrediente,Long>{
 }
--- src/main/java/br/com/restaurante/repository/ingredienteRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.restaurante.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ingredienteRepository
-    extends CrudRepository<ingrediente,long>{
-}
