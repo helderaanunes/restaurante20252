5b2cd30e5ef9c93e14bb65dc58d53252c866befb
AironLima <218002638+AironLima@users.noreply.github.com>
2025-09-02 20:15:46 -0300

    salvo

--- src/main/java/br/com/restaurante/controller/IngredienteController.java
+++ src/main/java/br/com/restaurante/controller/IngredienteController.java
@@ -1,9 +1,14 @@
 package br.com.restaurante.controller;
 
+import br.com.restaurante.entity.Ingrediente;
 import br.com.restaurante.service.IngredienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class IngredienteController {
     @Autowired
     private IngredienteService ingredienteService;
+
+    public void salvar (Ingrediente ingrediente){
+        ingredienteService.salvar(ingrediente);
+    }
 }
