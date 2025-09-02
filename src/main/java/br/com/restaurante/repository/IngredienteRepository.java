831e5cb93c0ebc16e281ea32dc6dfca0e9648bf0
AironLima <218002638+AironLima@users.noreply.github.com>
2025-09-02 19:14:17 -0300

    Refeito

---
+++ src/main/java/br/com/restaurante/repository/IngredienteRepository.java
@@ -0,0 +1,11 @@
+package br.com.restaurante.repository;
+
+import br.com.restaurante.entity.Ingrediente;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IngredienteRepository extends CrudRepository<Ingrediente,Long> {
+}
+
