14b7869371a0e85896e72a5b457002b24043d727
klaralcantara <92528150+klaralcantara@users.noreply.github.com>
2025-09-02 21:43:26 -0300

    Reserva salvar e listar

--- src/main/java/br/com/restaurante/controller/UsuarioController.java
+++ src/main/java/br/com/restaurante/controller/UsuarioController.java
@@ -1,4 +1,13 @@
 package br.com.restaurante.controller;
 
+import br.com.restaurante.service.ReservaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UsuarioController {
+
+    @Autowired
+    private ReservaService reservaService;
 }
+
