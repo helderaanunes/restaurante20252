4f1974b975d7776427e077e6cf859e0235c3b280
Kingbloom <nathanunes1881@gmail.com>
2025-09-02 21:38:55 -0300

    update ChaveAcessoMesa

--- src/main/java/br/com/restaurante/controller/ChaveAcessoMesaController.java
+++ src/main/java/br/com/restaurante/controller/ChaveAcessoMesaController.java
@@ -5,6 +5,7 @@ import br.com.restaurante.service.ChaveAcessoMesaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +19,10 @@ public class ChaveAcessoMesaController {
     @PostMapping("/chaveAcessoMesa")
     public ResponseEntity<ChaveAcessoMesa> salvar (@RequestBody ChaveAcessoMesa chaveAcessoMesa){
         return new ResponseEntity<ChaveAcessoMesa>(chaveAcessoMesaService.salvar(chaveAcessoMesa), HttpStatus.OK);
+    }
+
+    @GetMapping("/ChaveAcessoMesa")
+    public Iterable<ChaveAcessoMesa> listar(){
+        return chaveAcessoMesaService.listar();
     }
 }
--- src/main/java/br/com/restaurante/service/ChaveAcessoMesaService.java
+++ src/main/java/br/com/restaurante/service/ChaveAcessoMesaService.java
@@ -4,6 +4,7 @@ import br.com.restaurante.entity.ChaveAcessoMesa;
 import br.com.restaurante.repository.ChaveAcessoMesaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
 public class ChaveAcessoMesaService {
@@ -12,5 +13,9 @@ public class ChaveAcessoMesaService {
 
     public ChaveAcessoMesa salvar(ChaveAcessoMesa novo){
         return chaveAcessoMesaRepository.save(novo);
+    }
+
+    public Iterable<ChaveAcessoMesa> listar(){
+        return chaveAcessoMesaRepository.findAll();
     }
 }
