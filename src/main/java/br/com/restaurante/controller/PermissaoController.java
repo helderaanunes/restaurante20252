80e8f5de72dfc1a7b35865071d2e5d1df8660901
EdsonRivelino <edsonrivelino225@gmail.com>
2025-09-02 21:35:29 -0300

    .

--- src/main/java/br/com/restaurante/controller/PermissaoController.java
+++ src/main/java/br/com/restaurante/controller/PermissaoController.java
@@ -1,17 +1,32 @@
 package br.com.restaurante.controller;
 
 import br.com.restaurante.entity.Permissao;
-import br.com.restaurante.repository.PermissaoRepository;
+import br.com.restaurante.entity.Usuario;
+import br.com.restaurante.service.PermissaoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Service
-public class PermissaoService {
+@RestController
+public class PermissaoController {
 
     @Autowired
-    private PermissaoRepository permissaoRepository;
+    private PermissaoService permissaoService;
 
-    public Permissao salvar(Permissao novo){
-        permissaoRepository.save(novo);
+    @PostMapping("/permissao")
+    public ResponseEntity<Permissao> salvar(@RequestBody Permissao permissao){
+        return new ResponseEntity<Permissao>(
+        permissaoService.salvar(permissao)
+                , HttpStatus.OK);
+    }
+
+    @GetMapping("/permissao")
+    public Iterable<Permissao> listar(){
+        return permissaoService.listar();
     }
 }
+
