eb2f68c380c86de18f3d3af6b6a59db819b2655d
Kingbloom <nathanunes1881@gmail.com>
2025-09-02 21:14:54 -0300

    att

--- src/main/java/br/com/restaurante/controller/ChaveAcessoMesaController.java
+++ src/main/java/br/com/restaurante/controller/ChaveAcessoMesaController.java
@@ -1,12 +1,22 @@
 package br.com.restaurante.controller;
 
 import br.com.restaurante.entity.ChaveAcessoMesa;
+import br.com.restaurante.service.ChaveAcessoMesaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ChaveAcessoMesaController {
 
     @Autowired
-    private ChaveAcessoMesa chaveAcessoMesa;
+    private ChaveAcessoMesaService chaveAcessoMesaService;
+
+    @PostMapping("/chaveAcessoMesa")
+    public ResponseEntity<ChaveAcessoMesa> salvar (@RequestBody ChaveAcessoMesa chaveAcessoMesa){
+        return new ResponseEntity<ChaveAcessoMesa>(chaveAcessoMesaService.salvar(chaveAcessoMesa), HttpStatus.OK);
+    }
 }
