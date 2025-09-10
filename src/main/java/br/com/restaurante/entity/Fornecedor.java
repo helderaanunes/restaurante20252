77ec8cb89c5df3af4f7c984b2a1fbc761a1ee182
annyAtrSilva <anythalita51@gmail.com>
2025-09-09 21:47:39 -0300

    ajuste

--- src/main/java/br/com/restaurante/entity/Fornecedor.java
+++ src/main/java/br/com/restaurante/entity/Fornecedor.java
@@ -16,4 +16,44 @@ public class Fornecedor {
     private String cnpj;
     private String email;
     private String telefone;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
