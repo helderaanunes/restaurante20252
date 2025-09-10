fcf6db907166e1f06fa7e508706955253569fa64
Aluno <162748252+EdsonRivelino@users.noreply.github.com>
2025-09-09 21:24:34 -0300

    ajuste

--- src/main/java/br/com/restaurante/service/AtendimentoService.java
+++ src/main/java/br/com/restaurante/service/AtendimentoService.java
@@ -4,6 +4,8 @@ import br.com.restaurante.entity.Atendimento;
 import br.com.restaurante.repository.AtendimentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Optional;
+
 public class AtendimentoService {
 
     @Autowired
@@ -17,6 +19,17 @@ public class AtendimentoService {
     public Iterable<Atendimento> listar() {
         return atendimentoRepository.findAll();
     }
+    public Atendimento atualizar(Atendimento atendimento){
+        return atendimentoRepository.save(atendimento);
+    }
+    public void remover (Long id){
+        atendimentoRepository.deleteById(id);
+    }
+    public Optional<Atendimento> pegarPorId(Long id){
+        return atendimentoRepository.findById(id);
+    }
+
+
 
 }
 
