4bba92f82b6348b8bbae97af38c285793a6a98fb
Antonynns <93163044+Antonynns@users.noreply.github.com>
2025-09-09 21:15:30 -0300

    Conclusao do CRUD

--- src/main/java/br/com/restaurante/service/ItemCardapioService.java
+++ src/main/java/br/com/restaurante/service/ItemCardapioService.java
@@ -4,6 +4,8 @@ import br.com.restaurante.entity.ItemCardapio;
 import br.com.restaurante.repository.ItemCardapioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Optional;
+
 public class ItemCardapioService {
 
     @Autowired
@@ -15,5 +17,17 @@ public class ItemCardapioService {
 
     public Iterable<ItemCardapio> listar(){
         return itemCardapioRepository.findAll();
+    }
+
+    public ItemCardapio atualizar(ItemCardapio itemCardapio){
+        return itemCardapioRepository.save(itemCardapio);
+    }
+
+    public void remover(Long id){
+        itemCardapioRepository.deleteById(id);
+    }
+
+    public Optional<ItemCardapio> pegarPorId(Long id){
+        return itemCardapioRepository.findById(id);
     }
 }
