4d5842fc4f58a2990fc2744d2f325a3735626d92
Davidd-Rodrigues <davideaf2008@hotmail.com>
2025-09-16 21:36:28 -0300

    atualizei

--- src/main/java/br/com/restaurante/repository/IngredienteRepository.java
+++ src/main/java/br/com/restaurante/repository/IngredienteRepository.java
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 
->>>>>>> f16550b548b12d641d77741f77b5732e6ebddf21
 package br.com.restaurante.repository;
 
 import br.com.restaurante.entity.Ingrediente;
@@ -13,7 +10,3 @@ import org.springframework.stereotype.Repository;
 public interface IngredienteRepository extends CrudRepository<Ingrediente,Long> {
 }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> f16550b548b12d641d77741f77b5732e6ebddf21
