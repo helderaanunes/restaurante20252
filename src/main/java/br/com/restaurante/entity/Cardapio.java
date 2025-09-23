b49c55f8cab441859b07c8803d9aaf754131a34c
Thiago Natalicio <thiagonatalicio33@gmail.com>
2025-09-23 19:30:17 -0300

    gets and sets cardapio

--- src/main/java/br/com/restaurante/entity/Cardapio.java
+++ src/main/java/br/com/restaurante/entity/Cardapio.java
@@ -22,4 +22,51 @@ public class Cardapio {
     @Column (nullable = false)
     private Boolean visibilidade;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getInicioVigencia() {
+        return inicioVigencia;
+    }
+
+    public void setInicioVigencia(LocalDate inicioVigencia) {
+        this.inicioVigencia = inicioVigencia;
+    }
+
+    public boolean isExibirNoAltoAtendimento() {
+        return exibirNoAltoAtendimento;
+    }
+
+    public void setExibirNoAltoAtendimento(boolean exibirNoAltoAtendimento) {
+        this.exibirNoAltoAtendimento = exibirNoAltoAtendimento;
+    }
+
+    public LocalDate getFimVigencia() {
+        return fimVigencia;
+    }
+
+    public void setFimVigencia(LocalDate fimVigencia) {
+        this.fimVigencia = fimVigencia;
+    }
+
+    public Boolean getVisibilidade() {
+        return visibilidade;
+    }
+
+    public void setVisibilidade(Boolean visibilidade) {
+        this.visibilidade = visibilidade;
+    }
 }
