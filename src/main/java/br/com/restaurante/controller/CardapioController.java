208ae9a3df4cd2973699d961476a9a780b1dd5f8
Thaellany <senathaellany@gmail.com>
2025-09-26 21:54:17 -0300

    Cardapio Cors

--- src/main/java/br/com/restaurante/controller/CardapioController.java
+++ src/main/java/br/com/restaurante/controller/CardapioController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
+
 public class CardapioController {
     @Autowired
     private CardapioService cardapioService;
