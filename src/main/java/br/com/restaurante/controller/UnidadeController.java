3f5e1ccc31ef280b6210e78a7bf5298f53219d9c
Antonynns <93163044+Antonynns@users.noreply.github.com>
2025-10-07 21:01:00 -0300

    Correção de UnidadeController e CategoriaItemController

--- src/main/java/br/com/restaurante/controller/UnidadeController.java
+++ src/main/java/br/com/restaurante/controller/UnidadeController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-
+@CrossOrigin
 public class UnidadeController {
     @Autowired
     private UnidadeService unidadeService;
