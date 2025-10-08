fcb21b443a4a4e73d08614d0fa8458ed4b7c7e6f
Kingbloom <nathanunes1881@gmail.com>
2025-10-07 21:37:26 -0300

    update

--- src/main/java/br/com/restaurante/repository/ingredienteRepository.java
+++ src/main/java/br/com/restaurante/repository/ingredienteRepository.java
@@ -1,9 +1,10 @@
 package br.com.restaurante.repository;
 
+import br.com.restaurante.entity.SessaoClienteMesa;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ingredienteRepository
-    extends CrudRepository<ingrediente,long>{
+public interface IngredienteRepository
+        extends CrudRepository<SessaoClienteMesa,Long> {
 }
