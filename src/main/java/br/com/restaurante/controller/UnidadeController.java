8aeeb39c9f77641db94fb5ff71d4485504f38a3b
rafael4051 <rfledvaldo@gmail.com>
2025-10-07 21:11:19 -0300

    refeito

--- src/main/java/br/com/restaurante/controller/UnidadeController.java
+++ src/main/java/br/com/restaurante/controller/UnidadeController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-
+@CrossOrigin(origins = "*")
 public class UnidadeController {
     @Autowired
     private UnidadeService unidadeService;
