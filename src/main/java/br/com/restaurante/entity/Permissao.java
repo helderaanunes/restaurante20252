e732718935742624f1bf3c329b65eb0f76b183e8
victorA1ves <victoralve_s@outlook.com.br>
2025-10-07 21:22:49 -0300

    Corrigido o erro de Permissão - Victor, Clara e Gislene
    Foi adicinodo o Get e Set em Permissao.java(entity)

--- src/main/java/br/com/restaurante/entity/Permissao.java
+++ src/main/java/br/com/restaurante/entity/Permissao.java
@@ -9,6 +9,30 @@ import lombok.Data;
 @Entity
 @Data
 public class Permissao {
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
